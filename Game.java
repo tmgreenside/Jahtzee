ccb4c03ffacfa1a8793a5504a22fe5088d67ce39
tmgreenside <trevmg7@gmail.com>
2017-03-29 13:34:09 -0700

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,9 @@
+
+public class Game {
+
+	public static void main(String[] args) {
+		GameWindow theGame = new GameWindow();
+		theGame.printMessage();
+	}
+
+}
---
+++ GameWindow.java
@@ -0,0 +1,11 @@
+
+public class GameWindow {
+	String message;
+	public GameWindow() {
+		message = "Hello Bucky!";
+	}
+	
+	public void printMessage() {
+		System.out.println(message);
+	}
+}
