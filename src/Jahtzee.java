1893c2ecb3f30083c9b95d37dc1abed263ed99e0
tmgreenside <trevmg7@gmail.com>
2017-04-02 14:09:47 -0700

    Update Jahtzee.java

--- src/Jahtzee.java
+++ src/Jahtzee.java
@@ -1,7 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
 
 public class Jahtzee {
 
 	public static void main(String[] args) {
+		JFrame testFrame = new JFrame("Test");
+		testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		testFrame.setSize(200, 200);
+		Die theDie = new Die(6);
+		testFrame.add(theDie);
+		
+		testFrame.setVisible(true);
 		System.out.println("Guys, I think it works.");
 	}
 
