081e3d640e443060754c10765cc9f91aafc33d69
tmgreenside <trevmg7@gmail.com>
2017-04-17 14:09:00 -0700

    Create RulesWindow.java

---
+++ src/RulesWindow.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RulesWindow extends JFrame {
+	private JScrollPane scroller;
+	private JLabel rules1;
+	private JLabel rules2;
+	private JPanel rulesPanel;
+	
+	public RulesWindow() {
+		rules1 = new JLabel();
+		rules1.setIcon(new ImageIcon("images/Jahtzee_rules.png"));
+		rules2 = new JLabel();
+		rules2.setIcon(new ImageIcon("images/Jahtzee_rules_part2.png"));
+		
+		rulesPanel = new JPanel(new BorderLayout());
+		rulesPanel.add(rules1, BorderLayout.NORTH);
+		rulesPanel.add(rules2, BorderLayout.SOUTH);
+		scroller = new JScrollPane(rulesPanel);
+		
+		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		this.setSize(400, 600);
+		this.setTitle("Rules of Jahtzee");
+		this.add(scroller);
+	}
+}
