0779536f9e3e5ec54f0492e6ff6269abdc360f78
tmgreenside <trevmg7@gmail.com>
2017-04-17 19:04:39 -0700

    Update RulesWindow.java

--- src/RulesWindow.java
+++ src/RulesWindow.java
@@ -5,6 +5,8 @@ import java.awt.*;
  * This class creates a window displaying the document containing the 
  * rules of Jahtzee. 
  * @author Trevor Greenside
+ * @version Last Changed: 17 April 2017
+ * CPSC 224 Group Project: Jahtzee
  */
 public class RulesWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
